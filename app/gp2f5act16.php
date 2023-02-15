dd8660886e91d600c52cc7f2d8d38f7e29b65c82
vagrant <vagrant@gp2f5act16>
2023-02-15 19:19:58 +0000

    app_gp2f5act16 Versió 2.2

--- app/gp2f5act16.php
+++ app/gp2f5act16.php
@@ -1,8 +1,8 @@
 <!--app_gp2f5act16-->
 <!--Aplicació Versió 2.1-->
 <?php
-        function PreuMaquina($cpu,$cpu_tipus,$ram,$ssd,$sistema_operatiu,$ip,$acces) {
-                $suma = $cpu_tipus+$sistema_operatiu+$ip+$cpu+$ram+$ssd+$acces;
+        function PreuMaquina($cpu,$cpu_tipus,$ram,$ssd,$sistema_operatiu,$ip,$acces,$tipus_maquina) {
+                $suma = $cpu_tipus+$sistema_operatiu+$ip+$cpu+$ram+$ssd+$acces+$tipus_maquina;
                 echo "El preu total és: $suma €/mes";
                         // $fp = fopen("comandes.txt", "a");
                         // $fa = $_GET['codi'];
@@ -24,5 +24,5 @@
                         // fwrite($fp, $fh."\n");
                         // fclose($fp);
         }
-        echo PreuMaquina($_GET['cpu'],$_GET['cpu_tipus'],$_GET['ram'],$_GET['ssd'],$_GET['sistema_operatiu'],$_GET['ip'],$_GET['acces']);
+        echo PreuMaquina($_GET['cpu'],$_GET['cpu_tipus'],$_GET['ram'],$_GET['ssd'],$_GET['sistema_operatiu'],$_GET['ip'],$_GET['acces'],$_GET['tipus_maquina']);
 ?> 
