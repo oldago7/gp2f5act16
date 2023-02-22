fa7d7cd50a9c4e6fe0957d395faf35a81c950f1f
LAPTOP-ULMNNGPD\Olga <15587242.clot@fje.edu>
2023-02-22 20:19:23 +0100

    Versió 2.0

--- app/gp2f5act16.php
+++ app/gp2f5act16.php
@@ -1,35 +1,11 @@
 <!--app_gp2f5act16-->
-<!--Aplicació Versió 2.4-->
+<!--Aplicació Versió 2.0-->
 <?php
-        function PreuMaquina($cpu,$cpu_tipus,$ram,$ssd,$sistema_operatiu,$ip,$acces,$tipus_maquina,$banda_maxima,$compromis) {
-                $suma = $cpu_tipus+$sistema_operatiu+$ip+$cpu+$ram+$ssd+$acces+$tipus_maquina+$banda_maxima;
-		if ($compromis){
-			$porcentaje=$suma*10/100;
-                        $preu=$suma-$porcentaje;
-			echo "El preu total és: $preu €/mes";
-			}
-		else{
-			echo "El preu total és: $suma €/mes";
-			}
-                        // $fp = fopen("comandes.txt", "a");
-                        // $fa = $_GET['codi'];
-                        // $fb = $_GET['cpu'];
-                        // $fc = $_GET['cpu_tipus'];
-                        // $fd = $_GET['ram'];
-                        // $fe = $_GET['ssd'];
-                        // $ff = $_GET['sistema_operatiu'];
-                        // $fg = $_GET['ip'];
-                        // $fh = $_GET['acces'];
+        function PreuMaquina($cpu,$cpu_tipus,$ram,$ssd,$sistema_operatiu,$ip,$acces) {
+                $suma = $cpu_tipus+$sistema_operatiu+$ip+$cpu+$ram+$ssd+$acces;
 
-                        // fwrite($fp, $fa.", ");
-                        // fwrite($fp, $fb.", ");
-                        // fwrite($fp, $fc.", ");
-                        // fwrite($fp, $fd.", ");
-                        // fwrite($fp, $fe.", ");
-                        // fwrite($fp, $ff.", ");
-                        // fwrite($fp, $fg.", ");
-                        // fwrite($fp, $fh."\n");
-                        // fclose($fp);
+		echo "El preu total és: $suma €/mes";
+
         }
-        PreuMaquina($_GET['cpu'],$_GET['cpu_tipus'],$_GET['ram'],$_GET['ssd'],$_GET['sistema_operatiu'],$_GET['ip'],$_GET['acces'],$_GET['tipus_maquina'],$_GET['banda_maxima'],empty($_GET['compromis'])?false:true);
+        PreuMaquina($_GET['cpu'],$_GET['cpu_tipus'],$_GET['ram'],$_GET['ssd'],$_GET['sistema_operatiu'],$_GET['ip'],$_GET['acces']);
 ?> 
