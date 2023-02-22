92c325f49db53d7ab9a85159a2f5ec0567c4badc
LAPTOP-ULMNNGPD\Olga <15587242.clot@fje.edu>
2023-02-22 20:34:07 +0100

    Versió 2.1

--- app/gp2f5act16.php
+++ app/gp2f5act16.php
@@ -1,11 +1,11 @@
 <!--app_gp2f5act16-->
-<!--Aplicació Versió 2.0-->
+<!--Aplicació Versió 2.1-->
 <?php
-        function PreuMaquina($cpu,$cpu_tipus,$ram,$ssd,$sistema_operatiu,$ip,$acces) {
-                $suma = $cpu_tipus+$sistema_operatiu+$ip+$cpu+$ram+$ssd+$acces;
+        function PreuMaquina($cpu,$cpu_tipus,$ram,$ssd,$sistema_operatiu,$ip,$acces,$maquina) {
+                $suma = $cpu_tipus+$sistema_operatiu+$ip+$cpu+$ram+$ssd+$acces+$maquina;
 
 		echo "El preu total és: $suma €/mes";
 
         }
-        PreuMaquina($_GET['cpu'],$_GET['cpu_tipus'],$_GET['ram'],$_GET['ssd'],$_GET['sistema_operatiu'],$_GET['ip'],$_GET['acces']);
+        PreuMaquina($_GET['cpu'],$_GET['cpu_tipus'],$_GET['ram'],$_GET['ssd'],$_GET['sistema_operatiu'],$_GET['ip'],$_GET['acces'],$_GET['tipus_maquina']);
 ?> 
