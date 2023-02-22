594beabf2d41a1af4b2428cd322a6d527a355589
vagrant <vagrant@gp2f5act16>
2023-02-22 19:48:29 +0000

    Versió 2.2

--- app/gp2f5act16.php
+++ app/gp2f5act16.php
@@ -1,11 +1,11 @@
 <!--app_gp2f5act16-->
 <!--Aplicació Versió 2.1-->
 <?php
-        function PreuMaquina($cpu,$cpu_tipus,$ram,$ssd,$sistema_operatiu,$ip,$acces,$maquina) {
-                $suma = $cpu_tipus+$sistema_operatiu+$ip+$cpu+$ram+$ssd+$acces+$maquina;
+        function PreuMaquina($cpu,$cpu_tipus,$ram,$ssd,$sistema_operatiu,$ip,$acces,$maquina,$banda_maxima) {
+                $suma = $cpu_tipus+$sistema_operatiu+$ip+$cpu+$ram+$ssd+$acces+$maquina+$banda_maxima;
 
 		echo "El preu total és: $suma €/mes";
 
         }
-        PreuMaquina($_GET['cpu'],$_GET['cpu_tipus'],$_GET['ram'],$_GET['ssd'],$_GET['sistema_operatiu'],$_GET['ip'],$_GET['acces'],$_GET['tipus_maquina']);
+        PreuMaquina($_GET['cpu'],$_GET['cpu_tipus'],$_GET['ram'],$_GET['ssd'],$_GET['sistema_operatiu'],$_GET['ip'],$_GET['acces'],$_GET['tipus_maquina'],$_GET['banda_maxima']);
 ?> 
